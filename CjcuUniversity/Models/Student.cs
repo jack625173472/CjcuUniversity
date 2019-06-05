18092f923149eddbac9aee52a60e37bd402fdef7
jack625173472 <jack625173472@gmail.com>
2019-06-05 16:00:30 +0800

    0605~P2301_複雜model-1

--- CjcuUniversity/Models/Student.cs
+++ CjcuUniversity/Models/Student.cs
@@ -2,14 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CjcuUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "名字不能超過50個字元!")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime EnrollmentDate { get; set; }
 
         public virtual ICollection<Enrollment> Enrollments { get; set; }
